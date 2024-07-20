f86edcb79eb35963804e38e4268418126df6488a
“Brenno <dev.brennoeudes@gmail.com>
2024-07-20 10:59:37 -0300

    tailwind styles update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from "tailwindcss";
 
-const config: Config = {
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#000', 
+        secondary: '#212020',
+        tertiary: '#42393B',
+        quaternary: '#C6C6C6',
+      },
+      fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',  
+        base: '1rem',    
+        lg: '1.125rem', 
+        xl: '1.25rem',   
+        '2xl': '1.5rem', 
+        '3xl': '1.875rem', 
+        '4xl': '2.25rem', 
+        '5xl': '3rem',  
       },
     },
   },
   plugins: [],
 };
-export default config;
