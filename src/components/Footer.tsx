fdeda669f52d44726a97132585f6335f0c4d396c
“Brenno <dev.brennoeudes@gmail.com>
2024-07-23 15:43:42 -0300

    desktop footer layout concluded

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { fetchPayloadData } from "../services/api";
 
-import Image from "next/image";
-import instagramIcon from "../../public/assets/icons/instagram.svg";
-import facebookIcon from "../../public/assets/icons/fb-black.svg";
-import twitterIcon from "../../public/assets/icons/twitter.svg";
-
+import SocialLinks from "./SocialLinks";
 import FooterColumn from "./FooterColumn";
 
 export default function Footer() {
@@ -15,6 +11,7 @@ export default function Footer() {
     { title: string; links: { name: string; url: string }[] }[]
   >([]);
   const [error, setError] = useState<string | null>(null);
+  const [isLargeScreen, setIsLargeScreen] = useState<boolean>(false);
 
   useEffect(() => {
     const loadData = async () => {
@@ -61,6 +58,19 @@ export default function Footer() {
     loadData();
   }, []);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   if (error) {
     return <div className="text-red-500">{error}</div>;
   }
@@ -70,16 +80,18 @@ export default function Footer() {
   }
 
   return (
-    <footer className="px-4 pt-10">
-      <div className="container mx-auto flex flex-col gap-8 pt-6">
-        <h3>LOREM IPSUM</h3>
+    <footer className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 p-10 md:px-20 md:gap-20">
+      <div className="flex flex-col gap-4 md:flex-row md:items-start md:justify-between lg:w-[85%]">
+        {isLargeScreen ? (
+          <h2 className="flex-1">LOREM IPSUM</h2>
+        ) : (
+          <h3 className="flex-1">LOREM IPSUM</h3>
+        )}
         <div className="flex gap-6">
-          <Image src={instagramIcon} alt="Instagram icon" />
-          <Image src={facebookIcon} alt="Facebook icon" />
-          <Image src={twitterIcon} alt="X icon" />
+          <SocialLinks />
         </div>
       </div>
-      <div className="py-8 flex flex-wrap gap-8">
+      <div className="flex flex-col gap-8 md:flex-row md:flex-wrap md:items-start md:justify-between">
         {footerContent.map((section, index) => (
           <FooterColumn
             key={index}
